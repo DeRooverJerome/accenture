1b51acb13a3506d71617d3457a66ad1f8d93ef5e
Jerome <derooverjdr@gmail.com>
2024-02-14 10:39:23 +0100

    final ?

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,16 +1,19 @@
 import { Client, Account, Databases } from "appwrite";
 
+export const projectId = import.meta.env.PROJECT_ID;
+export const DBId = import.meta.env.DATABASE_ID;
+export const userCollectionId = import.meta.env.USER_COLLECTION_ID;
+export const clientCollectionId = import.meta.env.CLIENT_COLLECTION_ID;
+
+console.log("projectId", projectId);
+
 
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("65a6865a10c75ad51a2a");
+  .setProject(projectId);
 
 export const databases = new Databases(client);
 export { ID } from "appwrite";
-export const DBId = "65a68ade63aa62ea29c5";
-export const userCollectionId = "65b3b155c0c6d05b439a";
-export const clientCollectionId = "65c0a8f5eb3043371810";
-export const exampleUserId = "65b6705e625fa85f2bba";
 export const account = new Account(client);
 
 export const listUsers = () => {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+import react from "@vitejs/plugin-react";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // Expose environment variables to the Vite project
+    "import.meta.env.PROJECT_ID": JSON.stringify(process.env.PROJECT_ID),
+    "import.meta.env.DATABASE_ID": JSON.stringify(process.env.DATABASE_ID),
+    "import.meta.env.USER_COLLECTION_ID": JSON.stringify(
+      process.env.USER_COLLECTION_ID
+    ),
+    "import.meta.env.CLIENT_COLLECTION_ID": JSON.stringify(
+      process.env.CLIENT_COLLECTION_ID
+    ),
+  },
+});
